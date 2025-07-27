fcbcf485bbb3f3338d72984ac3bf3b8f6611b430
hongci9999 <hongci9999@gmail.com>
2025-07-27 23:45:30 +0900

    헤더 컴포넌트의 검색 입력 필드 스타일 수정 및 불필요한 SVG 아이콘 제거

--- fe/src/components/layout/Header.tsx
+++ fe/src/components/layout/Header.tsx
@@ -55,24 +55,9 @@ const Header = () => {
         <div className='relative'>
           <input
             type='text'
-            placeholder='맛집을 검색해보세요'
-            className='w-full px-4 py-2 pl-10 bg-gray-50 border border-gray-200 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent'
+            className='w-full px-4 py-2 pl-10 bg-gray-50 border-[3px] border-black rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent'
           />
-          <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
-            <svg
-              className='h-4 w-4 text-gray-400'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-              />
-            </svg>
-          </div>
+          <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'></div>
         </div>
       </div>
     </header>
